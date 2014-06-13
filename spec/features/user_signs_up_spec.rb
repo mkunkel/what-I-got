6a5bff9467ceecc35061a349947172f37acdeab3
JamesNix <jamesfnix@gmail.com>
2014-06-13 14:28:00 -0500

    Added user signs up spec

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+# feature "User signs up" do
+#   scenario "happy path" do
+#     visit '/'
+#     click_link
+#     click_link "I'm Ready!"
+#     pending
+#     fill_in "Email", with: "joe@example.com"
+#     fill_in "Username", with: "joe"
+#     fill_in "Password", with: "mypassword"
+#     fill_in "Password confirmation", with: "mypassword"
+#     click_button "Sign up"
+#     page.should have_content "Welcome to Squmblr, joe!"
+#     current_path.should == dashboard_path
+#   end
+# end
