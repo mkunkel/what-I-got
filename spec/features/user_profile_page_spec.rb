fbbbf906ac48fe7d31e7027b6fa239538fc3601d
JamesNix <jamesfnix@gmail.com>
2014-06-17 16:00:04 -0500

    Added profile page spec

---
+++ spec/features/user_profile_page_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature "User visits profile page" do
+  let!(:kate) { Fabricate(:user, email: "kate@example.com", username: "kate") }
+
+  scenario "views own profile" do
+    login_as kate
+    visit user_path(kate)
+    page.should have_content("This is your profile")
+    page.should have_content("What would you like to do?")
+    page.should have_content("Create a collection")
+    page.should have_content("View collections")
+  end
+
+end
