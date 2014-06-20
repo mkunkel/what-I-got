31fc7a84bf53432ef8a91ecce6c7ec42f7466742
JamesNix <jamesfnix@gmail.com>
2014-06-20 00:25:34 -0500

    Create book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140620050041) do
+ActiveRecord::Schema.define(version: 20140620051336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: true do |t|
+    t.string   "title",      default: "", null: false
+    t.string   "string"
+    t.string   "author"
+    t.string   "genre"
+    t.string   "rating"
+    t.string   "location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "collections", force: true do |t|
     t.datetime "created_at"
