9f87ba464a84daf45101aeca75a2c0672c18f902
JamesNix <jamesfnix@gmail.com>
2014-06-22 22:12:10 -0500

    Collection views styling

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -13,28 +13,4 @@ describe CollectionsController, :type => :controller do
     end
   end
 
-  describe "POST #create" do
-    describe "with valid params" do
-      it "saves the new collection in the database" do
-        expect {
-          post :create, {:collection => attributes_for(:collection)}
-        }.to change(Collection, :count).by(1)
-      end
-
-      it "redirects to the created collection" do
-        post :create, {:collection => attributes_for(:collection)}
-        response.should redirect_to(Collection.last)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved user as @collection" do
-        Collection.any_instance.stub(:save).and_return(false)
-        post :create, {:collection => { "title" => "invalid value" }}
-        assigns(:collection).should be_a_new(Collection)
-      end
-
-    end
-  end
-
 end
