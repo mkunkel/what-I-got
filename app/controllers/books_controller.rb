ad8c8df8ab5d13cfe09bf86b1b82c048ed4f0e2b
JamesNix <jamesfnix@gmail.com>
2014-06-25 14:56:37 -0500

    add book working properly

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,31 +3,28 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @collection = Collection.find(params[:collection_id])
   end
 
   def create
-    # @collection = Collection.find(params[:collection_id])
     @book = @collection.books.new(book_params)
     if @book.save
-      redirect_to(action: 'index')
+      redirect_to(action: "index")
     else
       render 'new'
     end
   end
 
   def new
-    # @collection = Collection.find(params[:collection_id])
-    @book = Books.new
+    @book = Book.new
   end
 
- private
-
-  def find_collection
-    @collection ||= Collection.find(params[:collection_id])
-  end
+  private
 
   def book_params
     params.require(:book).permit(:title, :author, :genre, :rating, :collection_id)
+  end
+
+  def find_collection
+    @collection ||= Collection.find(params[:collection_id])
   end
 end
